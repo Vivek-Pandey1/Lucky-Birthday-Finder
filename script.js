86e84db20010e39b032a48f93b6fcb1f081d09ef
Vivek Pandey <keviv2698@gmail.com>
2021-10-26 22:33:46 +0530

    chore: logic

---
+++ script.js
@@ -0,0 +1,35 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+function compareValues(sum,luckyNumber){
+  if(sum%luckyNumber===0){
+    outputBox.innerText="Your birthday is lucky 🚀";
+  }else {
+    outputBox.innerText="Your birthday is not lucky 🤒";
+  }
+}
+
+function checkBirthDateIsLucky(){
+  const dob =dateOfBirth.value;
+  const sum = calculateSum(dob);
+
+  if(luckyNumber.value && dob)
+  compareValues(sum,luckyNumber.value)
+  else 
+  outputBox.innerText="Please enter both the fields-_-";
+}
+
+function calculateSum(dob) {
+  dob= dob.replaceAll("-","");
+  let sum =0;
+  for(let index=0;index<dob.length;index++){
+    sum = sum + Number(dob.charAt(index));
+  }
+  return sum;
+}
+
+checkNumberButton.addEventListener('click',checkBirthDateIsLucky)
+
+// "2012-12-01"
