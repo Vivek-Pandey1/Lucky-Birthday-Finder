5562e43cd532ac3208e482db90668ea6d43917ee
Vivek Pandey <keviv2698@gmail.com>
2021-10-27 22:16:15 +0530

    doc: update

--- script.js
+++ script.js
@@ -3,33 +3,29 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
 const outputBox = document.querySelector("#output-box");
 
-function compareValues(sum,luckyNumber){
-  if(sum%luckyNumber===0){
-    outputBox.innerText="Your birthday is lucky 🙌";
-  }else {
-    outputBox.innerText="Your birthday is not lucky 🙁";
+function compareValues(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+    outputBox.innerText = "Your birthday is lucky 🙌";
+  } else {
+    outputBox.innerText = "Your birthday is not lucky 🙁";
   }
 }
 
-function checkBirthDateIsLucky(){
-  const dob =dateOfBirth.value;
+function checkBirthDateIsLucky() {
+  const dob = dateOfBirth.value;
   const sum = calculateSum(dob);
 
-  if(luckyNumber.value && dob)
-  compareValues(sum,luckyNumber.value)
-  else 
-  outputBox.innerText="Please enter both the fields 😑";
+  if (luckyNumber.value && dob) compareValues(sum, luckyNumber.value);
+  else outputBox.innerText = "Please enter both the fields 😑";
 }
 
 function calculateSum(dob) {
-  dob= dob.replaceAll("-","");
-  let sum =0;
-  for(let index=0;index<dob.length;index++){
+  dob = dob.replaceAll("-", "");
+  let sum = 0;
+  for (let index = 0; index < dob.length; index++) {
     sum = sum + Number(dob.charAt(index));
   }
   return sum;
 }
 
-checkNumberButton.addEventListener('click',checkBirthDateIsLucky)
-
-// "2012-12-01"
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky);
